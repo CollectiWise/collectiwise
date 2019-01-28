0101031226f2aa9e625511917fa8ffbfa89a8dcb
Ubuntu <ubuntu@ip-172-31-38-255.us-west-2.compute.internal>
2019-01-28 20:24:08 +0000

    reworking the message router to convert messages in various formats to Scheme Atomese

--- python/scheme_writer.py
+++ python/scheme_writer.py
@@ -1,67 +1,28 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 # 2019-01
 # Script to convert envo.obd to atomspace representation in scheme
 # Requires: file envo.obo from http://www.berkeleybop.org/ontologies/envo.obo
 from os.path import join
 import json
-
-class FileObject(object):
-    '''Base File Object from which InputFile and OutputFile inherit.'''
-
-    def __init__(self, filepath='./', filename='sample.txt', access='r+'):
-        # open a file filename in filepath in read and write mode
-        self.file = open(join(filepath, filename), access)
-
-    def __del__(self):
-        self.file.close()
-        del self.file
-
-class InputFile(FileObject):
-    '''Wrapper for our input file object.'''
-
-    def __init__(self, filename='envo.obo'):
-        super(InputFile, self).__init__(filename=filename)
-
-	self.obo   = self.file.readlines()
-    	self.line_no = self._line_no(self.obo)
-
-    def _line_no(self, obo):
-	line_nos = []
-    	for num, line in enumerate(obo, 1):
-	    if "[Term]" in line or "[Typedef]" in line:
-            	line_nos.append(num)
-	line_nos.sort()
-	return line_nos
-	
-	
-class OutputFile(FileObject):
-    """
-    basically at this stage just a wrapper around FileObject, but other interesting functionality may be added later
-    """
-    def __init__(self, filename='ENVO.scm'):
-        super(OutputFile, self).__init__(filename=filename, access='a')
-
-
-        
+import pronto
+       
 class SchemeWriter(object):
     
-    def __init__(self, outfile, processed=[], format='json'):
-	self.outfile = outfile
-	self.file    = outfile.file
-	self.input   = processed if format !='json' else self.all_to_terms(processed)
+    def __init__(self, ontology=None, format='json'):
+        self.ontology = ontology
 
 
     def write_all(self):
-	[self.term_to_scheme(term) for term in self.input]
+        [self.term_to_scheme(term) for term in self.input]
     
     def all_to_terms(self, list_of_json):
-	return [self.to_term(json_term) for json_term in list_of_json]
+        return [self.to_term(json_term) for json_term in list_of_json]
 
     def to_term(self, json_term):
-	return json_term
+        return json_term
 
     def print_all(self):
-	[self.term_to_scheme(term, write=False) for term in self.input]
+        [self.term_to_scheme(term, write=False) for term in self.input]
 
     def term_to_scheme(self, term, write=True): 
         [self.one_to_scheme(statement, write) for statement in term]   
@@ -73,37 +34,37 @@ class SchemeWriter(object):
 
         args = [idd, value]
         try:
-	    if write:
-	        self.write(key, args, indicator)
-	    else:
+            if write:
+                self.write(key, args, indicator)
+            else:
                 self.prnt(key, args, indicator)
-	except:
+        except:
             weird_cases[key]=value
 
     def writeOne(self, strings):
-	for st in strings:
-	    self.file.write(st)
+        for st in strings:
+            self.file.write(st)
 
     def printOne(self, strings):
-	print ''.join(strings)
+        print(''.join(strings))
 
     # methods to write on file or print scheme code:
     def inLink(self, node1 , node2, typ, link="inheritance", write=True):
         link_dict={"inheritance":"InheritanceLink", "subset":"SubsetLink"}
 	
-	strings   = ["("+ link_dict[link] + " \n", 
+        strings   = ["("+ link_dict[link] + " \n", 
 		"\t ("+typ+ " \"" + node1 + "\")\n", 
 		"\t ("+typ+" \""+ node2 + "\")\n", 
 		")\n\n"]
 
-    	if not write:
-	    self.printOne(strings)		
-	else:
-	    self.writeOne(strings)
+        if not write:
+            self.printOne(strings)		
+        else:
+            self.writeOne(strings)
     
     	
     def evaLink(self, predicateName ,node1 , node2 , node1_type, node2_type, write=True):
-	strings=["(EvaluationLink \n", 
+        strings=["(EvaluationLink \n", 
 		"\t (PredicateNode \"" + predicateName + "\")\n", 
 		"\t (ListLink \n", 
 		"\t\t (" + node1_type + " \"" + node1 + "\")\n",
@@ -111,15 +72,15 @@ class SchemeWriter(object):
     		"\t )\n",
     		")\n\n"]
 
-	if not write:
-	    self.printOne(strings)
-	else:
-	    self.writeOne(strings)
+        if not write:
+            self.printOne(strings)
+        else:
+            self.writeOne(strings)
 
 
 
     def to_scheme(self, key, args, indicator=1, write=True):
-	typ ="ConceptNode" if indicator else "PredicateNode"
+        typ ="ConceptNode" if indicator else "PredicateNode"
         """Dispatch method"""
         method_name = 'envo_' + str(key)
         # Get the method from 'self'. Default to a lambda.
@@ -128,23 +89,23 @@ class SchemeWriter(object):
         return method(*args, typ=typ, write=write)	#nd_assertion_to' # 'expand_expression_to' 
   
     def write(self, key, args, indicator=1):
-	self.to_scheme(key, args, indicator, write=True)
+        self.to_scheme(key, args, indicator, write=True)
     
     def prnt(self, key, args, indicator=1):
-	self.to_scheme(key, args, indicator, write=False) 
+        self.to_scheme(key, args, indicator, write=False) 
 
     def envo_id(self, idd, value, typ="ConceptNode", write=True):
-    	typp="ENVO_term" if (typ=="ConceptNode") else "ENVO_pred"
-    	self.inLink(idd, typp, typ=typ, write=write)
+        typp="ENVO_term" if (typ=="ConceptNode") else "ENVO_pred"
+        self.inLink(idd, typp, typ=typ, write=write)
     
     def envo_name(self, idd, name, typ="ConceptNode", write=True):
-    	self.evaLink("ENVO_name", idd, name, typ, "ConceptNode", write=write)
+        self.evaLink("ENVO_name", idd, name, typ, "ConceptNode", write=write)
 
     def envo_subset(self, idd, superset, typ="ConceptNode", write=True):
-    	self.inLink(idd, superset, typ = typ, link='subset', write=write)
+        self.inLink(idd, superset, typ = typ, link='subset', write=write)
 
     def envo_namespace(self, idd, namespace, typ="ConceptNode", write=True):
-    	self.evaLink("ENVO_namespace", idd, namespace , typ, "ConceptNode", write=write)
+        self.evaLink("ENVO_namespace", idd, namespace , typ, "ConceptNode", write=write)
 
     def envo_definition(self, idd, definition, typ="ConceptNode", write=True):
         self.evaLink("ENVO_definition", idd, definition ,typ, "ConceptNode", write=write)
@@ -153,13 +114,13 @@ class SchemeWriter(object):
         self.evaLink("ENVO_comment", idd, comment, typ, "ConceptNode", write=write)
  
     def envo_is_a(self, idd, is_a, typ="ConceptNode", write=True):	
-    	self.inLink(idd, is_a, typ=typ, write=write)
+        self.inLink(idd, is_a, typ=typ, write=write)
 
     def envo_alt_id(self, idd, alt_id, typ="ConceptNode", write=True):
         self.evaLink("ENVO_alt_id", idd, alt_id, typ, typ, write=write)
 
     def envo_relationship(self, idd, relate_id_type, typ="ConceptNode", write=True):
-	relate_id, relation_type =relate_id_type
+        relate_id, relation_type =relate_id_type
 
         self.evaLink(relation_type, idd, relate_id, typ , typ, write=write)
 
@@ -228,7 +189,7 @@ class SchemeWriter(object):
 mainstream={}
 all_else ={}
 weird_cases ={}
-
+relationships={}
 
 def main():
     #fi = InputFile()
@@ -240,7 +201,7 @@ def main():
 #    weird_cases ={}
 
     #terms=processor.process_all()
-    fo=OutputFile()
+    #fo=OutputFile()
     #writer=SchemeWriter(fo, terms)
     #writer.write_all()
 
@@ -284,12 +245,35 @@ def main():
             ]
         }
      })
-    writer=SchemeWriter(fo, [sample_json])
-    for inpt in writer.input:
-	term = json.loads(inpt)
-	outer_keys=set(term.keys()) -set(['other'])
-	print outer_keys, [(term['id'], 'id', term['id'])] + [(term['id'], key, val) for key, value in term['other'].items() for val in value if key!='id']
-        print term['relations']    
+    sd   = pronto.Ontology('https://raw.githubusercontent.com/SDG-InterfaceOntology/sdgio/master/sdgio.owl')
+    print('SDGIO:00000061' in sd)
+
+    #gaz = pronto.Ontology('http://ontologies.berkeleybop.org/gaz.obo')
+
+    envo = pronto.Ontology('https://raw.githubusercontent.com/EnvironmentOntology/envo/master/envo.obo')
+    print('SDGIO:00000061' in envo)
+    envo.merge(sd)
+    print('SDGIO:00000061' in envo)
+    force, parsers = envo._get_parsers(None)
+
+    print("force: ", force)
+    print("parsers: ", parsers)                                                                                                                                                                              
+    writer=SchemeWriter(envo, [sample_json])
+    for inpt in writer.ontology:
+        #print('term: ', eval(inpt.json)[inpt.id].keys())
+	
+        relation_names=set([key.obo_name for key in inpt.relations])
+        other_names = set(inpt.other.keys())-relation_names-set(['id', 'name', 'desc'])
+       
+        #for name in ['id', 'name', 'desc']:
+            
+        print(inpt.id, 'id', inpt.id)
+        print(inpt.id, 'name', inpt.name)
+        print(inpt.id, 'description', inpt.desc)
+        print(relation_names, other_names)
+        #print([(inpt.id, 'id', inpt.id)] + [(inpt.id, key, val) for key, value in inpt.other.items() for val in value if key!='id'])
+    #for r in relationships:
+    #    print(r.__dict__)
     
 
 if __name__ == "__main__":
